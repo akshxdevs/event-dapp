06bbb3fe6e73a8185f28c592dd93d09cea9a81f9
akshxdevs <akshxdevs@gmail.com>
2025-12-22 14:40:37 +0530

    project initialized

---
+++ rust-api/target/debug/deps/rust_api-f6212606d44990a6.d
@@ -0,0 +1,5 @@
+/home/akshxdevs/Codes/projects/web3/event-dapp/rust-api/target/debug/deps/rust_api-f6212606d44990a6.d: src/main.rs
+
+/home/akshxdevs/Codes/projects/web3/event-dapp/rust-api/target/debug/deps/librust_api-f6212606d44990a6.rmeta: src/main.rs
+
+src/main.rs:
