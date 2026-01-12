81c5b210d03253db1b8d4f44330491ab33d1ba29
Akash R <akshxdevs@gmail.com>
2026-01-12 13:09:21 +0530

    feat: file changes made

--- rust-api/target/debug/deps/rust_api-5ee7326267b82878.d
+++
@@ -1,5 +0,0 @@
-/home/akshxdevs/Codes/projects/web3/event-dapp/rust-api/target/debug/deps/rust_api-5ee7326267b82878.d: src/main.rs
-
-/home/akshxdevs/Codes/projects/web3/event-dapp/rust-api/target/debug/deps/librust_api-5ee7326267b82878.rmeta: src/main.rs
-
-src/main.rs:
--- rust-api/target/debug/deps/rust_api-a9b32163b9ab12fb.d
+++
@@ -1,5 +0,0 @@
-/home/akshxdevs/codes/projects/web3/event-dapp/rust-api/target/debug/deps/rust_api-a9b32163b9ab12fb.d: src/main.rs
-
-/home/akshxdevs/codes/projects/web3/event-dapp/rust-api/target/debug/deps/librust_api-a9b32163b9ab12fb.rmeta: src/main.rs
-
-src/main.rs:
